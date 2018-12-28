516d29805d1f6e5918eb76e560cd991e923d3b83
Martin Han <martinhan@martins-mbp-2.attlocal.net>
2018-12-28 08:44:02 -0800

    Display ··· between page numbers in reviews carousel

--- client/src/components/ReviewsPagesCarousel.jsx
+++ client/src/components/ReviewsPagesCarousel.jsx
@@ -2,38 +2,26 @@ import React from 'react';
 import _ from 'underscore';
 import ReviewsPagesCarouselNumberButton from './ReviewsPagesCarouselNumberButton.jsx';
 import ReviewsPagesCarouselDirectionButton from './ReviewsPagesCarouselDirectionButton.jsx';
+import ReviewsPagesCarouselEllipsis from './ReviewsPagesCarouselEllipsis.jsx';
 
 const ReviewsPagesCarousel = (props) => {
   const totalPages = Math.ceil(props.selectedReviews.length / props.reviewsPerPage);
-  let pageNumbers = [];
-  pageNumbers.push(1, props.currentReviewsPage - 1, props.currentReviewsPage, props.currentReviewsPage + 1, totalPages)
+  let pageNumbers = [1, props.currentReviewsPage - 1, props.currentReviewsPage, props.currentReviewsPage + 1, totalPages];
   pageNumbers = _.uniq(pageNumbers); // remove duplicates
-  pageNumbers = pageNumbers.filter(num => ( // remove pagenumbers out of range
-    num >= 1 && num <= totalPages
-  ));
+  pageNumbers = pageNumbers.filter(num => ( num >= 1 && num <= totalPages )); // remove pagenumbers out of range
 
-  if (props.currentReviewsPage === 1 && !pageNumbers.includes(3)) {
+  if (props.currentReviewsPage === 1 && !pageNumbers.includes(3)) { // if current page = 1, add page number 3
     pageNumbers.splice(2, 0, 3);
-  } else if (props.currentReviewsPage === totalPages && !pageNumbers.includes(totalPages - 2)) {
+  } else if (props.currentReviewsPage === totalPages && !pageNumbers.includes(totalPages - 2)) { // if current page = n, add page # n - 2
     pageNumbers.splice(pageNumbers.length - 2, 0, totalPages - 2);
   }
 
-  // if ([1, 2].includes(props.currentReviewsPage)) {
-  //   pageNumbers.splice(3, 1);
-  // } else if ([totalPages -1, totalPages].includes(props.currentReviewsPage)) {
-  //   pageNumbers.splice(1, 1);
-  // }
+  for (let i = 0; i < pageNumbers.length; i++) {
+    if ((pageNumbers[i] !== '···') && (pageNumbers[i] !== pageNumbers[i + 1] - 1) && (i !== pageNumbers.length - 1)) {
+      pageNumbers.splice(i + 1, 0, '···');
+    }
+  }
 
-
-  // if pageNumber = 1, 2, n - 1, or n
-    // max number of pagenums displayed = 4
-    // remove duplicates and pagenums out of range
-  // else
-    // max number of pagenums displayed = 5
-    // remove duplicates and pagenums out of range
-  // add "..." between 1 and m - 1 and/or m + 1 and n if not consecutive numbers
-
-  // pageNumbers.map (pageNumber => {reviewspagescarouselnumberbutton})
   return (
     <div id="reviewspagecarousel">
       <ReviewsPagesCarouselDirectionButton
@@ -43,6 +31,8 @@ const ReviewsPagesCarousel = (props) => {
       />
       <div id="pagenumberbuttonslist">
         {pageNumbers.map((pageNumber, i) => (
+          pageNumber === '···' ? 
+          <ReviewsPagesCarouselEllipsis /> :
           <ReviewsPagesCarouselNumberButton
             pageNumber={pageNumber}
             updateReviewsPage={props.updateReviewsPage}
---
+++ client/src/components/ReviewsPagesCarouselEllipsis.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ReviewsPagesCarouselEllipsis = (props) => (
+  <div className="pagenumberellipsis">
+    ···
+  </div>
+);
+
+export default ReviewsPagesCarouselEllipsis;
