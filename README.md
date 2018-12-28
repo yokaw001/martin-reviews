bbbf14cf2d363f16f29d55497875cca514d52477
Martin Han <martinhan@martins-mbp-2.attlocal.net>
2018-12-28 08:51:31 -0800

    Add icon next to user review count for each review

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 Restaurant reviews component for OpenTable product page
 
 # TO DOS
-  Reviews List pagination - finish pagination functionality (add "..."s)
   "Read More" button functionality for long comments
   Review username - add random color property, add "VIP" flag for random users, add comments icon
   Reviews Summary - build ratings graph, "loved for" list
--- client/src/components/ReviewUser.jsx
+++ client/src/components/ReviewUser.jsx
@@ -5,7 +5,10 @@ const ReviewUser = (props) => (
     <div className="userinitials"><span>{props.userInitials}</span></div>
     <div className="username">{props.username}</div>
     <div className="usercity">{props.userCity}</div>
-    <div className="userreviewcount">{props.reviewCount + ' reviews'}</div>
+    <div className="userreviewcount">
+      <img className="userreviewcounticon" src="https://s3-us-west-1.amazonaws.com/gitbuckets/hrla26-fec-tableit/tableit_review_user_comments_icon.png"></img>
+      <span>{props.reviewCount + ' reviews'}</span>
+    </div>
   </div>
 );
 
