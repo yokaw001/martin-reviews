89e9f817fde41b82814b4464e4d9abc29c05263a
Martin Han <martinhan@martins-mbp-2.attlocal.net>
2018-12-28 08:59:57 -0800

    Add random color property to username initials

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Restaurant reviews component for OpenTable product page
 
 # TO DOS
+  Review username - add "VIP" flag for random users
   "Read More" button functionality for long comments
-  Review username - add random color property, add "VIP" flag for random users, add comments icon
   Reviews Summary - build ratings graph, "loved for" list
   Add CSS modules
--- client/src/components/ReviewUser.jsx
+++ client/src/components/ReviewUser.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
-const ReviewUser = (props) => (
-  <div className="reviewuser">
-    <div className="userinitials"><span>{props.userInitials}</span></div>
-    <div className="username">{props.username}</div>
-    <div className="usercity">{props.userCity}</div>
-    <div className="userreviewcount">
-      <img className="userreviewcounticon" src="https://s3-us-west-1.amazonaws.com/gitbuckets/hrla26-fec-tableit/tableit_review_user_comments_icon.png"></img>
-      <span>{props.reviewCount + ' reviews'}</span>
+const ReviewUser = (props) => {
+  const colors = ['#df4e96', '#6c8ae4', '#d86441', '#bb6acd'];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+  return (
+    <div className="reviewuser">
+      <div style={{'background-color':randomColor}} className="userinitials"><span>{props.userInitials}</span></div>
+      <div className="username">{props.username}</div>
+      <div className="usercity">{props.userCity}</div>
+      <div className="userreviewcount">
+        <img className="userreviewcounticon" src="https://s3-us-west-1.amazonaws.com/gitbuckets/hrla26-fec-tableit/tableit_review_user_comments_icon.png"></img>
+        <span>{props.reviewCount + ' reviews'}</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ReviewUser;
