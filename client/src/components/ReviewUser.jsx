4b92ecb5b6d5b8f16be56577b65c024cab53428e
Martin Han <martinhan@martins-mbp-2.attlocal.net>
2018-12-28 23:04:16 -0800

    Fix bug where username color and VIP status changes every render

--- client/src/components/ReviewUser.jsx
+++ client/src/components/ReviewUser.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const ReviewUser = (props) => {
   const colors = ['#df4e96', '#6c8ae4', '#d86441', '#bb6acd'];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  const randomNumber = Math.random();
+  const randomColor = colors[(props.username.length % colors.length)];
+  const randomDigit = parseInt(props.username.length.toString().split('').pop());
 
   return (
     <div className="reviewuser">
-      {randomNumber < 0.3 ? <div className="vip"><span>VIP</span></div> : null}
+      {randomDigit < 3 ? <div className="vip"><span>VIP</span></div> : null}
       <div style={{'background-color':randomColor}} className="userinitials">
         <span>{props.userInitials}</span>
       </div>
