e13c1942222511a7e4ce890521462c1845a3bb3e
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-02-05 10:04:59 -0800

    [prog] revert mysql promise package

--- db/index.js
+++ db/index.js
@@ -1,9 +1,9 @@
-const mysql = require('promise-mysql');
-// const mysql = require('mysql-promise');
+// const mysql = require('promise-mysql');
+const mysql = require('mysql-promise');
 const config = require('./config.js');
 
+// const db = mysql.createConnection(config);
 const db = mysql();
-
 db.configure(config);
 
 module.exports = db;
